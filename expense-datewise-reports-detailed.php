25f57934b3c8342df039b0bf790f642de6ed856d
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2020-12-08 19:22:41 -0800

    Update expense-datewise-reports-detailed.php

--- expense-datewise-reports-detailed.php
+++ expense-datewise-reports-detailed.php
@@ -18,7 +18,7 @@ if (strlen($_SESSION['id']==0)) {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>PaySmith || Datewise Expense Report</title>
+	<title>PaySmith || Day-to-Day Transaction Report</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/datepicker3.css" rel="stylesheet">
@@ -31,9 +31,11 @@ if (strlen($_SESSION['id']==0)) {
 <body>
   <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
         <div class="profile-sidebar">
-            <div class="profile-userpic">
-                <img src="http://placehold.it/50/30a5ff/fff" class="img-responsive" alt="">
-            </div>
+            <!--
+                  <div class="profile-userpic">
+                  <img src="http://placehold.it/50/30a5ff/fff" class="img-responsive" alt="">
+              </div>
+              -->
             <div class="profile-usertitle">
                 <?php
 $uid=$_SESSION['id'];
@@ -55,14 +57,14 @@ $name=$row['fname'];
 
 
             <li class="parent "><a data-toggle="collapse" href="#sub-item-1">
-                <em class="fa fa-navicon">&nbsp;</em>Expenses <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+                <em class="fa fa-navicon">&nbsp;</em>Transactions <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
                 </a>
                 <ul class="children collapse" id="sub-item-1">
                     <li><a class="" href="add-expense.php">
-                        <span class="fa fa-arrow-right">&nbsp;</span> Add Expenses
+                        <span class="fa fa-arrow-right">&nbsp;</span> Add Transaction
                     </a></li>
                     <li><a class="" href="manage-expense.php">
-                        <span class="fa fa-arrow-right">&nbsp;</span> Manage Expenses
+                        <span class="fa fa-arrow-right">&nbsp;</span> Edit Transaction(s)
                     </a></li>
 
                 </ul>
@@ -70,17 +72,17 @@ $name=$row['fname'];
             </li>
 
   <li class="parent "><a data-toggle="collapse" href="#sub-item-2">
-                <em class="fa fa-navicon">&nbsp;</em>Expense Report <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+                <em class="fa fa-navicon">&nbsp;</em> Transaction Report <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
                 </a>
                 <ul class="children collapse" id="sub-item-2">
                     <li><a class="" href="expense-datewise-reports.php">
-                        <span class="fa fa-arrow-right">&nbsp;</span> Daywise Expenses
+                        <span class="fa fa-arrow-right">&nbsp;</span> Day-to-Day
                     </a></li>
                     <li><a class="" href="expense-monthwise-reports.php">
-                        <span class="fa fa-arrow-right">&nbsp;</span> Monthwise Expenses
+                        <span class="fa fa-arrow-right">&nbsp;</span> Month-to-Month
                     </a></li>
                     <li><a class="" href="expense-yearwise-reports.php">
-                        <span class="fa fa-arrow-right">&nbsp;</span> Yearwise Expenses
+                        <span class="fa fa-arrow-right">&nbsp;</span> Year-to-Year
                     </a></li>
 
                 </ul>
@@ -104,7 +106,7 @@ $name=$row['fname'];
 				<li><a href="#">
 					<em class="fa fa-home"></em>
 				</a></li>
-				<li class="active">Datewise Expense Report</li>
+				<li class="active">Day-to-Day Transaction Report</li>
 			</ol>
 		</div><!--/.row-->
 
@@ -117,7 +119,7 @@ $name=$row['fname'];
 
 
 				<div class="panel panel-default">
-					<div class="panel-heading">Datewise Expense Report</div>
+					<div class="panel-heading">Day-to-Day Transaction Report</div>
 					<div class="panel-body">
 
 						<div class="col-md-12">
@@ -127,7 +129,7 @@ $fdate=$_POST['fromdate'];
  $tdate=$_POST['todate'];
 $rtype=$_POST['requesttype'];
 ?>
-<h5 align="center" style="color:blue">Datewise Expense Report from <?php echo $fdate?> to <?php echo $tdate?></h5>
+<h5 align="center" style="color:blue">Day-to-Day Transaction Report from <?php echo $fdate?> to <?php echo $tdate?></h5>
 <hr />
                                     <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                         <thead>
