596107edaf3ceff318520d253ae3067df185928e
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2020-12-08 18:53:51 -0800

    Update add-expense.php

--- add-expense.php
+++ add-expense.php
@@ -89,14 +89,14 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
 
 
               <li class="parent "><a data-toggle="collapse" href="#sub-item-1">
-                  <em class="fa fa-navicon">&nbsp;</em>Expenses <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+                  <em class="fa fa-navicon">&nbsp;</em>Transactions <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
                   </a>
                   <ul class="children collapse" id="sub-item-1">
                       <li><a class="" href="add-expense.php">
-                          <span class="fa fa-arrow-right">&nbsp;</span> Add Expenses
+                          <span class="fa fa-arrow-right">&nbsp;</span> Add Transaction
                       </a></li>
                       <li><a class="" href="manage-expense.php">
-                          <span class="fa fa-arrow-right">&nbsp;</span> Manage Expenses
+                          <span class="fa fa-arrow-right">&nbsp;</span> Edit Transaction(s)
                       </a></li>
 
                   </ul>
@@ -104,17 +104,17 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
               </li>
 
     <li class="parent "><a data-toggle="collapse" href="#sub-item-2">
-                  <em class="fa fa-navicon">&nbsp;</em>Expense Report <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+                  <em class="fa fa-navicon">&nbsp;</em>Transaction Report <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
                   </a>
                   <ul class="children collapse" id="sub-item-2">
                       <li><a class="" href="expense-datewise-reports.php">
-                          <span class="fa fa-arrow-right">&nbsp;</span> Daywise Expenses
+                          <span class="fa fa-arrow-right">&nbsp;</span> Day-to-Day
                       </a></li>
                       <li><a class="" href="expense-monthwise-reports.php">
-                          <span class="fa fa-arrow-right">&nbsp;</span> Monthwise Expenses
+                          <span class="fa fa-arrow-right">&nbsp;</span> Month-to-Month
                       </a></li>
                       <li><a class="" href="expense-yearwise-reports.php">
-                          <span class="fa fa-arrow-right">&nbsp;</span> Yearwise Expenses
+                          <span class="fa fa-arrow-right">&nbsp;</span> Year-to-Year
                       </a></li>
 
                   </ul>
