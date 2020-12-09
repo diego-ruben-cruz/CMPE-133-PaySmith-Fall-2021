1634b668272cb6a4be80da7f43ed811c36da5ab3
hungnguyen4785 <62048685+hungnguyen4785@users.noreply.github.com>
2020-12-08 18:20:39 -0800

    Add files via upload

--- expense-yearwise-reports-detailed.php
+++ expense-yearwise-reports-detailed.php
@@ -22,7 +22,7 @@ if (strlen($_SESSION['id']==0)) {
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/datepicker3.css" rel="stylesheet">
-	<link href="css/styles.css" rel="stylesheet">
+	<link href="css/styles2.css" rel="stylesheet">
 
 	<!--Custom Font-->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
