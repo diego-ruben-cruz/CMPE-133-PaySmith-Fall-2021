780b828d7b4cf64ce7de1693a5dfa10fae959efe
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2020-12-08 19:46:33 -0800

    Update manage-expense.php

--- manage-expense.php
+++ manage-expense.php
@@ -17,7 +17,7 @@ $rowid=intval($_GET['delid']);
 $query=mysqli_query($conn,"delete from transactions where id='$rowid'");
 if($query){
 echo "<script>alert('Record successfully deleted');</script>";
-echo "<script>window.location.href='manage-expense.php'</script>";
+echo "<script>window.location.href='manage-Transactions.php'</script>";
 } else {
 echo "<script>alert('Something went wrong. Please try again');</script>";
 
@@ -32,7 +32,7 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Pay Smith || Manage Expense</title>
+	<title>PaySmith || Edit Transaction(s)</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/datepicker3.css" rel="stylesheet">
@@ -92,10 +92,10 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
                     <em class="fa fa-navicon">&nbsp;</em>Transactions <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
                     </a>
                     <ul class="children collapse" id="sub-item-1">
-                        <li><a class="" href="add-expense.php">
+                        <li><a class="" href="add-Transactions.php">
                             <span class="fa fa-arrow-right">&nbsp;</span> Add Transaction
                         </a></li>
-                        <li><a class="" href="manage-expense.php">
+                        <li><a class="" href="manage-Transactions.php">
                             <span class="fa fa-arrow-right">&nbsp;</span> Edit Transaction(s)
                         </a></li>
 
@@ -107,13 +107,13 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
                     <em class="fa fa-navicon">&nbsp;</em>Transaction Report <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
                     </a>
                     <ul class="children collapse" id="sub-item-2">
-                        <li><a class="" href="expense-datewise-reports.php">
+                        <li><a class="" href="Transactions-datewise-reports.php">
                             <span class="fa fa-arrow-right">&nbsp;</span> Day-to-Day
                         </a></li>
-                        <li><a class="" href="expense-monthwise-reports.php">
+                        <li><a class="" href="Transactions-monthwise-reports.php">
                             <span class="fa fa-arrow-right">&nbsp;</span> Month-to-Month
                         </a></li>
-                        <li><a class="" href="expense-yearwise-reports.php">
+                        <li><a class="" href="Transactions-yearwise-reports.php">
                             <span class="fa fa-arrow-right">&nbsp;</span> Year-to-Year
                         </a></li>
 
@@ -138,7 +138,7 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
 				<li><a href="#">
 					<em class="fa fa-home"></em>
 				</a></li>
-				<li class="active">Expense</li>
+				<li class="active">Transactions</li>
 			</ol>
 		</div><!--/.row-->
 
@@ -151,7 +151,7 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
 
 
 				<div class="panel panel-default">
-					<div class="panel-heading">Expense</div>
+					<div class="panel-heading">Transactions</div>
 					<div class="panel-body">
 						<p style="font-size:16px; color:red" align="center"> <?php if($msg){
     echo $msg;
@@ -163,9 +163,9 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
               <thead>
                 <tr>
                   <th>S.NO</th>
-                  <th>Expense Item</th>
-                  <th>Expense Cost</th>
-                  <th>Expense Date</th>
+                  <th>Transactions Item</th>
+                  <th>Transactions Cost</th>
+                  <th>Transactions Date</th>
                   <th>Action</th>
                 </tr>
               </thead>
@@ -183,7 +183,7 @@ while ($row=mysqli_fetch_array($ret)) {
                   <td><?php  echo $row['item'];?></td>
                   <td><?php  echo $row['cost'];?></td>
                   <td><?php  echo $row['date'];?></td>
-                  <td><a href="manage-expense.php?delid=<?php echo $row['id'];?>">Delete</a>
+                  <td><a href="manage-Transactions.php?delid=<?php echo $row['id'];?>">Delete</a>
                 </tr>
                 <?php
 $cnt=$cnt+1;
