5bfb7199dc40a485d3343dd3b269b2dcd475ef7a
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2020-12-10 14:13:42 -0800

    Update expense-reports.php

--- expense-reports.php
+++ expense-reports.php
@@ -102,7 +102,7 @@ if (strlen($_SESSION['id']==0)) {
 
 
               <li class="parent "><a data-toggle="collapse" href="#sub-item-3">
-                  <em class="fa fa-navicon">&nbsp;</em> Crowdfund <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+                  <em class="fa fa-navicon">&nbsp;</em> Crowdfunds <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
                   </a>
                   <ul class="children collapse" id="sub-item-3">
                       <li><a class="" href="add-crowdfund.php">
